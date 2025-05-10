84e7aaa4b3d946afabe022566e8c51c3ed578137
dazenpc <121750352+dazenpc@users.noreply.github.com>
2025-05-10 22:50:42 +0530

    feat: add nadvbar links loader

---
+++ src/navbar_links_loader.js
@@ -0,0 +1,4 @@
+let allTasks = document.querySelector(".quick-links li:first-child");
+let todayTasks = document.querySelector(".quick-links li:nth-child(2)");
+let upcomingTasks = document.querySelector(".quick-links li:nth-child(3)");
+let completedTasks = document.querySelector(".quick-links li:nth-child(4)");
